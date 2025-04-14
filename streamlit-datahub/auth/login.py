67f9031a9a33bcd9727ea4c0dc1e21e68def7156
Rerisson H R Silva <148692810+RerissonHRS@users.noreply.github.com>
2025-04-14 09:29:52 -0300

    Add files via upload

--- streamlit-datahub/auth/login.py
+++ streamlit-datahub/auth/login.py
@@ -4,16 +4,16 @@ import streamlit_authenticator as stauth
 def create_authenticator():
     names = ["Usuário Teste"]
     usernames = ["usuario"]
-    passwords = ["123"]
-
-    hashed_passwords = stauth.Hasher(passwords).generate()
+    hashed_passwords = [
+        "$2b$12$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    ]
 
     authenticator = stauth.Authenticate(
         names,
         usernames,
         hashed_passwords,
         "datahub_app",
-        "abcdef",
+        "abcdef",  # Um segredo qualquer
         cookie_expiry_days=1
     )
     return authenticator
